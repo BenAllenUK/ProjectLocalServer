18311389db358821e5acd208bdc3a3b1e5bf31b9
Ben Allen <me@benallen.info>
2018-04-04 15:40:29 +0100

    output

--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
 cd /home/pi/Desktop/ProjectLocalServer
 echo $$
 while true; do
-  python3 check-script.py
+  stdbuf -oL python3 check-script.py > /home/pi/Desktop/ProjectLocalServer/py-server.log
   git reset --hard HEAD
   git pull
   sleep 1h
