695777431b6c529e853e6a6d3d7d6f82e40b9e93
Ben Allen <me@benallen.info>
2018-04-05 16:37:43 +0100

    auto confirm

--- install.sh
+++ install.sh
@@ -9,17 +9,17 @@ sudo systemctl start ssh
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install screen mc vim git htop
+sudo apt-get -y install screen mc vim git htop
 
 wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -
-sudo apt-get install apt-transport-https
-sudo apt-get install libudev-dev
+sudo apt-get -y install apt-transport-https
+sudo apt-get -y install libudev-dev
 sudo adduser openhab dialout
 sudo adduser openhab tty
 #RESTART
 echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list
 sudo apt-get update
-sudo apt-get install openhab2
+sudo apt-get -y install openhab2
 
 sudo /etc/init.d/openhab2 start
 sudo /etc/init.d/openhab2 status
